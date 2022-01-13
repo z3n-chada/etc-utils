774b99ad6634b0940f9d76f3d5af4a69019f07e3
z3n <tyler@ethereum.org>
2022-01-13 11:43:41 -0600

    add lighthouse testnet generation.

--- modules/python/TestnetDirectoryGenerator.py
+++ modules/python/TestnetDirectoryGenerator.py
@@ -8,6 +8,7 @@ import os
 import pathlib
 import shutil
 import subprocess
+from itertools import zip_longest
 
 from ruamel import yaml
 
@@ -50,7 +51,7 @@ class TestnetDirectoryGenerator(object):
         # of the typical list format
         cmd = (
             f"eth2-val-tools keystores --out-loc {target_dir} "
-            + f"--source-min 0 --source-max {self.num_validators-1} "
+            + f"--source-min 0 --source-max {self.num_validators} "
             + f'--source-mnemonic "{self.mnemonic}"'
         )
         if password is not None:
@@ -68,7 +69,7 @@ class TestnetDirectoryGenerator(object):
             val_dir = f"node_{x}"
             cmd = (
                 f"eth2-val-tools keystores --out-loc {target_dir}/{val_dir} "
-                + f"--source-min 0 --source-max {self.num_validators-1} "
+                + f"--source-min 0 --source-max {self.num_validators} "
                 + f'--source-mnemonic "{self.mnemonic}"'
             )
             if password is not None:
@@ -143,9 +144,59 @@ def create_prysm_testnet_dirs(global_config, docker):
     pdg = PrysmTestnetDirectoryGenerator(global_config, docker=docker)
     pdg.generate_testnet_dir()
 
+
+class LighthouseTestnetDirectoryGenerator(TestnetDirectoryGenerator):
+    def __init__(self, global_config, docker=True):
+        super().__init__(global_config)
+        if docker:
+            self.testnet_dir = pathlib.Path(
+                self.global_config["pos-chain"]["clients"]["lighthouse"][
+                    "docker-testnet-dir"
+                ]
+            )
+        else:
+            self.testnet_dir = pathlib.Path(
+                self.global_config["pos-chain"]["clients"]["lighthouse"]["host-testnet-dir"]
+            )
+        self.validator_store_dir = pathlib.Path(str(self.testnet_dir) + "/validators/")
+        self.testnet_dir.mkdir()
+
+    def generate_testnet_dir(self):
+        print("Creating lighthouse testnet directory...")
+        # create local geneis ssz and config.yaml
+        shutil.copy(src=self.genesis_ssz, dst=str(self.testnet_dir) + "/genesis.ssz"),
+        shutil.copy(src=self.config, dst=str(self.testnet_dir) + "/config.yaml")
+        self.generate_all_validators(self.validator_store_dir)
+        # split up into subdirs.
+        divisor = int(self.num_validators / self.num_nodes)
+        curr_offset = 0
+        print("Organizing lighthouse testnet beacon node directories..")
+        # use secrets to get all the keys.
+        all_key_paths = pathlib.Path(str(self.validator_store_dir) + "/secrets/").glob('*')
+        sub_dir_keys = zip_longest(*[iter(all_key_paths)]*divisor)
+        for ndx, keys in enumerate(sub_dir_keys):
+            node_dir = pathlib.Path(str(self.testnet_dir) + f"/node_{ndx}/")
+            node_dir.mkdir()
+            secrets_dir = pathlib.Path(str(self.testnet_dir) + f"/node_{ndx}/secrets/")
+            secrets_dir.mkdir()
+            for k in keys:
+                #validators then secrets
+                src_key_dir = str(self.validator_store_dir)+f"/keys/{k.name}"
+                dst_key_dir = str(node_dir) + f'/{k.name}'
+                shutil.copytree(src_key_dir, dst_key_dir)
+                dst_secret_key = str(secrets_dir)+f'/{k.name}'
+                shutil.copy(k, dst_secret_key)
+        shutil.rmtree(str(self.testnet_dir) + "/validators/")
+
+def create_lighthouse_testnet_dirs(global_config, docker):
+    lhdg = LighthouseTestnetDirectoryGenerator(global_config, docker=docker)
+    lhdg.generate_testnet_dir()
+
 def create_client_testnet_dir(global_config, client, docker):
     if client == 'prysm':
         create_prysm_testnet_dirs(global_config, docker)
+    elif client == 'lighthouse':
+        create_lighthouse_testnet_dirs(global_config, docker)
     else:
         raise Exception(f"Unimplented client label for testnet directory creation ({client})")
 
