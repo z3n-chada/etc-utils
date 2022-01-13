51e37698c5a5f1bcf2c3437219bd0374d90729ec
z3n <tyler@ethereum.org>
2022-01-13 10:38:10 -0600

    provide keystore stuff for geth

---
+++ scripts/geth-get-keystore.py
@@ -0,0 +1,33 @@
+from ruamel import yaml
+import json
+from web3.auto import w3
+
+w3.eth.account.enable_unaudited_hdwallet_features()
+
+'''
+    generate keyfiles for geth genesis..
+'''
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", dest="config", help="path to config to consume")
+
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f.read())
+
+    mnemonic = config["pow-chain"]["accounts"]["eth1-account-mnemonic"]
+    password = config["pow-chain"]["accounts"]["eth1-passphrase"]
+    premines = config["pow-chain"]["accounts"]["eth1-premine"]
+    for acc in premines:
+        acct = w3.eth.account.from_mnemonic(
+            mnemonic, account_path=acc, passphrase=password
+        )
+        print(acct.address)
+        print(acct.privateKey)
+        keyfile = acct.encrypt(password)
+        with open(f'keyfile-{acct.address}.key', 'w') as f:
+            json.dump(keyfile, f)
